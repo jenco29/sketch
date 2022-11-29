a6cad9e654ae218d1ed9ad46cb939043385db5f0
Jennie Cohen <jenniecohen181@gmail.com>
2022-11-29 19:58:34 +0000

    intermediate tests pass

--- sketch.c
+++ sketch.c
@@ -45,32 +45,65 @@ int getOperand(byte b) {
 void obey(display *d, state *s, byte op) {
   int opcode = getOpcode(op);
   int operand = getOperand(op);
-  if(opcode == 2) s->tool = operand;
-  else if(opcode == 0) {
+  if(opcode == 2) {
+    if(operand < 3) s->tool = operand;
+    else{
+      if(operand == 3) colour(d, s->data);
+      else if(operand == 4) s->tx = s->data;
+      else if(operand == 5) s->ty = s->data;
+      s->data = 0;
+    }
+  } // tool
+  else if(opcode == 0) { // update x
     s->tx += operand;
   }
-  else if(opcode == 1) {
+  else if(opcode == 3) { // data
+    s->data = (s->data << 6) | (operand & 63);
+  }
+  else if(opcode == 1) { // update y
     s->ty += operand;
-    if(s-> tool != 0) line(d, s->x, s->y, s->tx, s->ty);
+    if(s->tool == 1) {
+      line(d, s->x, s->y, s->tx, s->ty);
+      s->data = 0;
+    }
+    else if(s->tool == 2) {
+      block(d, s->x, s->y, s->tx - s->x, s->ty - s->y);
+      s->data = 0;
+    }
     s->x = s->tx;
     s->y = s->ty;
-}
   }
+}
 
 // Draw a frame of the sketch file. For basic and intermediate sketch files
 // this means drawing the full sketch whenever this function is called.
 // For advanced sketch files this means drawing the current frame whenever
 // this function is called.
 bool processSketch(display *d, const char pressedKey, void *data) {
-
+  if (data == NULL) return (pressedKey == 27);
+  char *filename = getName(d);
+  state *s = (state*) data;
+  FILE *in = fopen(filename, "rb");
+  byte b = fgetc(in);
+  obey(d, s, b);
+  while(! feof(in)){
+    b = fgetc(in);
+    obey(d, s, b);
+  }
+  fclose(in);
+  show(d);
+  s->x = 0;
+  s->y = 0;
+  s->tx = 0;
+  s->ty = 0;
+  s->tool = 1;
+  return true;
     //TO DO: OPEN, PROCESS/DRAW A SKETCH FILE BYTE BY BYTE, THEN CLOSE IT
     //NOTE: CHECK DATA HAS BEEN INITIALISED... if (data == NULL) return (pressedKey == 27);
     //NOTE: TO GET ACCESS TO THE DRAWING STATE USE... state *s = (state*) data;
     //NOTE: TO GET THE FILENAME... char *filename = getName(d);
     //NOTE: DO NOT FORGET TO CALL show(d); AND TO RESET THE DRAWING STATE APART FROM
     //      THE 'START' FIELD AFTER CLOSING THE FILE
-
-  return (pressedKey == 27);
 }
 
 // View a sketch file in a 200x200 pixel window given the filename
